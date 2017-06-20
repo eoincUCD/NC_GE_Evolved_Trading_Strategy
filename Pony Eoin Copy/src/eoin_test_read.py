6e0338773fabde82e325e1547abb732045517d82
Eoin Carroll <eoin.carroll1@ucdconnect.ie>
2017-06-20 16:47:37 +0100

    Backup

--- Pony Eoin Copy/src/eoin_test_read.py
+++ Pony Eoin Copy/src/eoin_test_read.py
@@ -1,14 +1,18 @@
 import pandas as pd
 import random
+import time
 from math import floor
 
 random.seed(12345)
 in_file = "../../data/AAPL UW Equity.csv"
 df = pd.read_csv(in_file)
 
+out_file_name = "../../logs/" + time.strftime("%Y%m%d-%H%M%S") + "_test_fitness.txt"  # Log file name
+out_file = open(out_file_name, "w")  # Open log file
+
 # start at first data point in 2000
 # We have 17.5 years worth of data and will ignore 2017 (the 0.5yr)
-# Split data into 16 years with the 17th being a test case
+# Split data into 17 years with the 17th being a test case
 
 d = {}
 d["points"] = int((len(df) - 105) / 17)  # Number of data points available per year . . . we have 17 years of data
@@ -18,7 +22,7 @@ d["shares"] = 0
 for i in range(16):  # Loop 16 times - one for each year of data . . . roughly
     start = i * d["points"]  # Start of that year
     end = (i+1) * d["points"]  # End of that year
-    print(i, start, end)
+    # print(i, start, end)
     # start = 1968
     # end = 2214
     for j in range(start, end):  # Loop for each day in that year
@@ -32,8 +36,8 @@ for i in range(16):  # Loop 16 times - one for each year of data . . . roughly
 
         # todo exec on this point
 
-        out = random.random()
-        if out < 0.5:
+        d["out"] = random.random()
+        if d["out"] < 0.5:
             d["recommendation"] = "buy"
         else:
             d["recommendation"] = "sell"
@@ -46,7 +50,12 @@ for i in range(16):  # Loop 16 times - one for each year of data . . . roughly
             d["cash"] = d["cash"] + d["shares"] * d["last_price"]
             d["shares"] = 0
 
+        out_file.write(str(i) + " " + str(j) + str(d) + "\n")
+
     d["cash"] = d["cash"] + d["shares"] * d["last_price"]
     d["shares"] = 0
     d["XXX_output_XXX"] = d["cash"]
-print(": Final result, cash remaining after 10000:", d["XXX_output_XXX"])
+
+out_file.write("Final result, cash remaining after 10000:" + d["XXX_output_XXX"] + "\n")
+print("Final result, cash remaining after 10000:", d["XXX_output_XXX"])
+out_file.close()
